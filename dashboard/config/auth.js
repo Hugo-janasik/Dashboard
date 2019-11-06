2f2218cb5570c3407a475e160bd00ab00fb2c02c
Hugo Janasik <hjanasik@hugo-computer.home>
2019-11-06 23:49:02 +0100

    ADD Another CSS Template, REPLACE handlebars file by ejs file, ADD Facebook auth, Simplify the code

---
+++ dashboard/config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'facebookAuth': {
+        'clientID': '577128116366065',
+        'clientSecret': 'b6130d2cf6a901dbdb8b69c7b8ff17a6',
+        'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+    }
+}
